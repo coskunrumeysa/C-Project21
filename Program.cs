18c7f6b113e91509c11d4529a754fa7ff2857ab2
coskunrumeysa <70705198+coskunrumeysa@users.noreply.github.com>
2022-06-01 11:10:40 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿//Beni Yaratan Elbet Yolumu Gösterir .Şuara78
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte number1;
+            number1 = 123;
+
+            Console.WriteLine("Number is:" + " " + number1);
+
+            Console.Read();
+        }
+    }
+}
